2f70ad11b1f3f974a3c9afffefcd8e6e2e330da0
AnwarMestycer <anwar.mestycer@tekniva.net>
2023-01-01 15:04:36 +0100

    add cart functionalities to search-bar component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, TitleStrategy } from '@angular/router';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,13 +10,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+private _cart: Cart = {items: []};
+itemsQuantity = 0;
+@Input()
+get cart():Cart{
+  return this._cart;
+}
+set cart(cart: Cart){
+  this._cart = cart;
+  this.itemsQuantity = cart.items
+  .map((item) => item.quantity)
+  .reduce((prev, current) => prev + current, 0)
+}
+  constructor(private router:Router, private cartService:CartService) { }
 
-  constructor(private router:Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
   onSubmit(form: NgForm){
     this.router.navigate(['search', form.value.search])
+  }
+
+  getTotal(items : Array<CartItem>): number{
+    return this.cartService.getTotal(items);
+  }
+  clearCart(){
+    return this.cartService.clearCart();
   }
 
 }
